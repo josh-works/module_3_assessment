ec4d1612f69efe6a43843d96c421b1714ba0dcbd
Josh Thompson <thompsonjoshd@gmail.com>
2017-06-01 10:34:01 -0600

    all tests pass, but API call still ugly AF

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,13 +1,8 @@
 class StoresController < ApplicationController
 
   def index
-    @all_stores = Store.find_by_zip
+    @all_stores = Store.find_by_zip(params[:Search])
 
-    # response = Faraday.get('https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=name,distance,phone,storeType&pageSize=100&apiKey=uf3hrj9pa5u2jxvqa3xfq5a5')
-    # results = JSON.parse(response.body, symbolize_names: true)[:stores]
-    # @all_stores = results.map do |store|
-    #   Store.new(store)
-    # end
     @stores = @all_stores[0..9]
 
   end
